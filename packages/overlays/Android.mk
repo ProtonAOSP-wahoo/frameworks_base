856aa2672d2f56379ed5759bca81677444ba2014
Pranav Vashi <neobuddy89@gmail.com>
2022-02-13 05:33:34 +0530

    overlays: Add Outline iconpack
    :)

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -60,6 +60,9 @@ LOCAL_REQUIRED_MODULES := \
     IconPackOOSSettingsOverlay \
     IconPackOOSSystemUIOverlay \
     IconPackOOSThemePickerOverlay \
+    IconPackOutlineAndroidOverlay \
+    IconPackOutlineSettingsOverlay \
+    IconPackOutlineSystemUIOverlay \
     IconPackAcherusAndroidOverlay \
     IconPackAcherusSystemUIOverlay \
     IconShapeCloudyOverlay \
